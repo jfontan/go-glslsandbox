da5f7081d92e30ed2e95038f8eb4b82be98fb661
Javi Fontan <jfontan@gmail.com>
2019-07-07 19:49:26 +0200

    Load effects from json
    Signed-off-by: Javi Fontan <jfontan@gmail.com>

---
+++ database.go
@@ -0,0 +1,75 @@
+package glsl
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Effect struct {
+	gorm.Model
+
+	ID            uint `json:"_id" gorm:"primary_key"`
+	Created       time.Time
+	Modified      time.Time
+	Parent        *Effect
+	ParentID      uint `json:"parent" sql:"default:null"`
+	ParentVersion int  `json:"parent_version"`
+	User          string
+	Versions      []Version
+}
+
+type effectJSON struct {
+	Effect
+
+	Versions   []versionJSON
+	CreatedAt  Timestamp `json:"created_at" gorm:"-"`
+	ModifiedAt Timestamp `json:"modified_at" gorm:"-"`
+}
+
+type Version struct {
+	gorm.Model
+
+	Number  int
+	Created time.Time
+	Code    string
+}
+
+type versionJSON struct {
+	Version
+	CreatedAt Timestamp `json:"created_at" gorm:"-"`
+}
+
+type Timestamp struct {
+	Date int64 `json:"$date"`
+}
+
+func (t Timestamp) Time() time.Time {
+	seconds := t.Date / 1000
+	milis := t.Date % 1000
+	return time.Unix(seconds, milis*1000000)
+}
+
+func (e *effectJSON) convert() {
+	e.Created = e.CreatedAt.Time()
+	e.Modified = e.ModifiedAt.Time()
+
+	for i, ver := range e.Versions {
+		ver.Number = i
+		ver.Created = ver.CreatedAt.Time()
+		e.Effect.Versions = append(e.Effect.Versions, ver.Version)
+	}
+}
+
+func LoadEffect(text []byte) (*Effect, error) {
+	var effect effectJSON
+	err := json.Unmarshal(text, &effect)
+	if err != nil {
+		return nil, err
+	}
+
+	effect.convert()
+
+	return &effect.Effect, nil
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/jfontan/go-glslsandbox
+
+go 1.12
+
+require (
+	cloud.google.com/go v0.41.0 // indirect
+	github.com/denisenkom/go-mssqldb v0.0.0-20190707035753-2be1aa521ff4 // indirect
+	github.com/jinzhu/gorm v1.9.10
+	github.com/stretchr/testify v1.3.0
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+)
